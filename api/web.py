1dd91c5ec867b8f980fcb76a56883263035424e5
welann <49773825+welann@users.noreply.github.com>
2021-10-13 16:05:42 +0800

    Update web.py

--- api/web.py
+++ api/web.py
@@ -8,10 +8,10 @@ def catch_all(path):
     print(request.args)
     key = request.args.get('key')
 #     data=json.dumps({"code":200,"msg":"success","newslist":[{"content":"我不允许你们说自己胖 你们那不叫胖 那叫可爱到膨胀。"}]})
-#     return Response(data, mimetype="text/json")
-    user1 = {"name":"nick"}
-    data = json.dumps(user1)
-    return data,200,{"ContentType":"application/json"}
+    return Response("我不允许你们说自己胖 你们那不叫胖 那叫可爱到膨胀。", mimetype="text/json")
+#     user1 = {"name":"nick"}
+#     data = json.dumps(user1)
+#     return data,200,{"ContentType":"application/json"}
     
 if __name__ == "__main__":
     app.run(debug = True)
