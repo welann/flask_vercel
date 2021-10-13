298f11cf2cdb642d06784e56ff3445dff0a596c8
welann <49773825+welann@users.noreply.github.com>
2021-10-13 16:37:11 +0800

    Update web.py

--- api/web.py
+++ api/web.py
@@ -1,17 +1,18 @@
 from flask import Flask, Response, request
+from random import choice
 
 app = Flask(__name__)
+
+
+list=["让人迷茫的原因只有一个，那就是本该拼搏的年纪，想的太多，却做的太少！","你知道吗，你红尘梦醒的那一刻，是我看过的，最美丽的画面。",\
+      "跟你在一起的时光都很耀眼,因为天气好,因为天气不好,因为天气刚刚好,每一天，都很美好。","凌晨四点钟，我看见海棠花未眠。总觉得这时，你应该在我身边。",\
+     "一个人至少拥有一个梦想，有一个理由去坚强。心若没有栖息的地方，到哪里都是在流浪。","无论靠近还是远离，最后的结果都是难过。","被大雨淋湿时就想想晒干衣服的阳光和彩虹歉意的微笑"]
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    print(request.args)
-    key = request.args.get('key')
-#     data=json.dumps({"code":200,"msg":"success","newslist":[{"content":"我不允许你们说自己胖 你们那不叫胖 那叫可爱到膨胀。"}]})
-    return Response("我不允许你们说自己胖 你们那不叫胖 那叫可爱到膨胀。", mimetype="text/json")
-#     user1 = {"name":"nick"}
-#     data = json.dumps(user1)
-#     return data,200,{"ContentType":"application/json"}
     
+    return Response(choice(list), mimetype="text/json")
+
 if __name__ == "__main__":
     app.run(debug = True)
